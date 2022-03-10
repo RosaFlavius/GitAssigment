d47c06b7d986d67591c7a70863b3ebc50006fbe8
RosaFlavius <flavius.rosa@amdaris.com>
2022-03-10 12:54:07 +0200

    Conflict commit

--- Prg/Prg/Program.cs
+++ Prg/Prg/Program.cs
@@ -6,7 +6,7 @@ namespace MyApp // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Conflict user2!");
+            Console.WriteLine("Conflict user2!!");
             Console.WriteLine("Hello World!");
         }
     }
